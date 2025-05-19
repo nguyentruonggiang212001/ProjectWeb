99adac2adc97cc8f87507a16ced6d51be16d8fcc
nguyentruonggiang212001 <nguyentruonggiang212001@gmail.com>
2025-05-19 17:37:18 +0700

    Backend

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -6,9 +6,6 @@ const url = env.MONGO_URI;
 const connectDB = async () => {
   try {
     const connected = await mongoose.connect(url);
-    console.log(
-      `Connected MongoDB: mongodb://${connected.connection.host}:${connected.connection.port}/${connected.connection.name}`
-    );
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
