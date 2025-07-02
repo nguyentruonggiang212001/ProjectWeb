16f5ad4ec32a7e8e1fbaba9f5aa8f589ebf4c833
nguyentruonggiang212001 <nguyentruonggiang212001@gmail.com>
2025-07-02 17:55:44 +0700

    Backend

--- Backend/validations/categoryValid.js
+++ Backend/validations/categoryValid.js
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const categorySchema = z.object({
-  title: z.string().min(1, "Title is required"),
+  title: z.string().min(1, "Phải có tiêu đề"),
   description: z.string().optional(),
   slug: z.string().optional(),
 });
